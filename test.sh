ded8b5a05e87d7eb97882b0aedce4943fee0c8f9
Robert Grzelka <robert.grzelka@ericsson.com>
2019-07-02 14:40:03 +0200

    Fix permissions, add verbosity to test

--- test.sh
+++ test.sh
@@ -1,7 +1,14 @@
-curl -H Host: nginx.example localhost
-curl -H Host: whoami.local localhost
-curl -H Host: whoami.local localhost
-curl -H Host: whoami.local localhost
-curl -H Host: whoami.local localhost
-curl -H Host: whoami.local localhost
-curl -H Host: proxy.example localhost
+python -c "print('#'*30, 'test nginx [#1] reponse')"
+curl -H "Host: nginx.example" localhost
+curl -H "Host: nginx.example" localhost
+curl -H "Host: nginx.example" localhost
+python -c "print('#'*30, 'test whoami [#3] reponse')"
+curl -H "Host: whoami.local"  localhost
+curl -H "Host: whoami.local"  localhost
+curl -H "Host: whoami.local"  localhost
+curl -H "Host: whoami.local"  localhost
+curl -H "Host: whoami.local"  localhost
+python -c "print('#'*30, 'test http-server [#1] reponse')"
+curl -H "Host: proxy.example" localhost
+curl -H "Host: proxy.example" localhost
+curl -H "Host: proxy.example" localhost
