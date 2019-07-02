0aa6f9ebf4b2eb3c83be3297f690fd33990f0289
Robert Grzelka <robert.grzelka@ericsson.com>
2019-07-02 14:27:14 +0200

    First commit.

---
+++ test.sh
@@ -0,0 +1,7 @@
+curl -H Host: nginx.example localhost
+curl -H Host: whoami.local localhost
+curl -H Host: whoami.local localhost
+curl -H Host: whoami.local localhost
+curl -H Host: whoami.local localhost
+curl -H Host: whoami.local localhost
+curl -H Host: proxy.example localhost
